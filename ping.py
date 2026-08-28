e64395c9dda09f0c580f6fd2a789e3a598182fb8
Matteo Vicentini <145768623+matteovice89@users.noreply.github.com>
2026-08-28 12:10:44 +0200

    Update ping script to check IP reachability
    Refactor ping script to read IPs from a file and check reachability.

--- ping.py
+++ ping.py
@@ -1,21 +1,28 @@
+*** elenco estratto da dhcp server di windows server, prendo ip per ip testo se è raggiungibile ***
+import platform
 import subprocess
+ip_list = []
+with open('elenco.txt', 'r', encoding='utf-8') as f:
+    for riga in f:
+        inizio = riga.find('[')
+        fine = riga.find(']')
+        if inizio != -1 and fine != -1:
+            ip = riga[inizio + 1 : fine]
+            
 
-f = open('nuovoElenco.txt','r') #apertura file
-
-for linea in f:
-    destinazione=linea[:11]
+    
 
 
-    comando = ["ping", '-n', '1', destinazione]
+            comando = ["ping", '-n', '1', ip]
 
-    risultato = subprocess.run(comando, capture_output=True, text=True)
-    with open('raggiungibili.txt','a') as ff :
-        if risultato.returncode == 0:
-            scrivi=(linea + " RAGGIUNGIBILE \n")
-            ff.write(scrivi)
-        else:
-             scrivi=(linea + " NON RAGGIUNGIBILE \n")
-             ff.write(scrivi)
+            risultato = subprocess.run(comando, capture_output=True, text=True)
+            with open('raggiungibili.txt','a') as ff :
+                if risultato.returncode == 0:
+                    scrivi=(riga + " RAGGIUNGIBILE \n")
+                    ff.write(scrivi)
+                else:
+                     scrivi=(riga + " NON RAGGIUNGIBILE \n")
+                     ff.write(scrivi)
 
 f.close()
 
